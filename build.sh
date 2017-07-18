f6a149108e71306c8114bc6256a4ae880698ee27
Randy Fay <randy@randyfay.com>
2017-07-18 10:34:31 -0600

    ln the right way

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 export GOPATH=/tmp/go
 DRUDSRC=$GOPATH/src/github.com/drud
 mkdir -p $DRUDSRC
-ln -s $DRUDSRC/ddev $PWD
+ln -s $PWD $DRUDSRC/ddev 
 cd $DRUDSRC/ddev
 make test
