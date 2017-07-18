e4be072c9a3c6c106ea3861573974eb3ad534f4d
Randy Fay <randy@randyfay.com>
2017-07-18 11:44:01 -0600

    Try full test

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# This script is used to build drud/ddev using surf 
+# (https://github.com/surf-build/surf)
+
+# Manufacture a $GOPATH environment that can mount on docker
 BUILD=$(date "+%Y%m%d%H%M%S")
 export GOPATH=~/tmp/ddevbuild_$BUILD
 DRUDSRC=$GOPATH/src/github.com/drud
@@ -16,4 +20,4 @@ ln -s $PWD $DRUDSRC/ddev
 cd $DRUDSRC/ddev
 
 #make print-BUILD_OS print-DDEV_BINARY_FULLPATH
-make testcmd
+time make test
