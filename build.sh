2cd6f63d63ff2c782dba3af2e06d943e00022c4e
Randy Fay <randy@randyfay.com>
2017-07-18 11:23:06 -0600

    More debugging

--- build.sh
+++ build.sh
@@ -2,13 +2,15 @@
 
 BUILD=$(date "+%Y%m%d%H%M%S")
 export GOPATH=~/tmp/ddevbuild_$BUILD
+DRUDSRC=$GOPATH/src/github.com/drud
+
+echo "Building in $DRUDSRC/ddev"
 
 export GOTEST_SHORT=1
 
 echo "Warning: deleting all docker containers"
 docker rm -f $(docker ps -aq) 2>/dev/null || true
 
-DRUDSRC=$GOPATH/src/github.com/drud
 mkdir -p $DRUDSRC
 ln -s $PWD $DRUDSRC/ddev 
 cd $DRUDSRC/ddev
