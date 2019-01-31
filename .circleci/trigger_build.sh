9e737e3a40f5edc38076654d9d1a38e4dd8be216
Randy Fay <randy@randyfay.com>
2019-01-31 15:40:16 -0700

    Make sure we actually use the tag

--- .circleci/trigger_build.sh
+++ .circleci/trigger_build.sh
@@ -17,9 +17,13 @@ RELEASE_TAG=${6:-}
 trigger_build_url=https://circleci.com/api/v1.1/project/github/$PROJECT/tree/$BRANCH?circle-token=${CIRCLE_TOKEN}
 
 BUILD_PARAMS="\"CIRCLE_JOB\": \"${CIRCLE_JOB}\", \"GITHUB_TOKEN\":\"${GITHUB_TOKEN}\", \"RELEASE_TAG\": \"${RELEASE_TAG}\""
+if [ "${RELEASE_TAG}" != "" ]; then
+    DATA="\"tag\": \"$RELEASE_TAG\","
+fi
+DATA="${DATA} { \"build_parameters\": \"${BUILD_PARAMS}\" } "
 
 curl -sS \
   --header "Content-Type: application/json" \
-  --data "{\"build_parameters\": {${BUILD_PARAMS}}}" \
+  --data "${DATA}" \
   --request POST \
     $trigger_build_url
