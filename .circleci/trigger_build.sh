6b2e5e5b523b02592fee98df15ff967015632d27
Randy Fay <randy@randyfay.com>
2019-01-31 11:53:42 -0700

    Use circle's repo names [skip ci] [ci skip]

--- .circleci/trigger_build.sh
+++ .circleci/trigger_build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "This script is currently inoperative because we use CircleCI workflows, which can't be started via API" && exit 1
-
 # from https://circleci.com/docs/1.0/nightly-builds/
 # See also https://circleci.com/docs/2.0/defining-multiple-jobs/
 
