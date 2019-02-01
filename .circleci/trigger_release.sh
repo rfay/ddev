e074d7e64b45794d6ae21697e2e9a531d311e07f
Randy Fay <randy@randyfay.com>
2019-01-31 17:21:59 -0700

    Improving docs

--- .circleci/trigger_release.sh
+++ .circleci/trigger_release.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# trigger_release.sh $circle_token $project_optional $branch_optional $github_token_optional $release_tag_optional
+# trigger_release.sh $circle_token $release_tag $github_token $project_optional
 
-# .circleci/trigger_build.sh c4e304eb5323f91900908b3443ecirclekey release_build rfay/ddev 20190131_deploy_artifacts_from_circle 9f622120dd9800f5c59ea80a1ad9f7c353962dea  v1.5.9  | jq -r 'del(.circle_yml)'
+# .circleci/trigger_release.sh circlepikey0908b3443ea58316baf928b v1.5.9 githubpersonaltokenc590a1ad9f7c353962dea optional/ddev  | jq -r 'del(.circle_yml)'
 
 # api docs: https://circleci.com/docs/api
 # Trigger a new job: https://circleci.com/docs/api/v1-reference/#new-build
 
 CIRCLE_TOKEN=$1
-PROJECT=${2:-drud/ddev}
+RELEASE_TAG=${2:-}
 GITHUB_TOKEN=${3:-}
-RELEASE_TAG=${4:-}
+PROJECT=${4:-drud/ddev}
 
 trigger_build_url=https://circleci.com/api/v1.1/project/github/$PROJECT?circle-token=${CIRCLE_TOKEN}
 
