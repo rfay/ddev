38619f6cf65d4d61d63787425e94ab9ccb0fef2d
Randy Fay <randy@randyfay.com>
2022-12-18 16:44:05 -0700

    Experimental bump of base web image to bookworm

--- pkg/versionconstants/versionconstants.go
+++ pkg/versionconstants/versionconstants.go
@@ -17,7 +17,7 @@ var SegmentKey = ""
 var WebImg = "drud/ddev-webserver"
 
 // WebTag defines the default web image tag
-var WebTag = "v1.21.4" // Note that this can be overridden by make
+var WebTag = "20221218_debian_bookworm" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "drud/ddev-dbserver"
