1550458f80ce24522d029c26f60607f259e3cd40
Randy Fay <randy@randyfay.com>
2024-02-23 09:57:53 -0700

    Kill colima by opening too many clients [skip ci]

--- cmd/ddev/main.go
+++ cmd/ddev/main.go
@@ -1,28 +1,20 @@
 package main
 
 import (
+	"fmt"
+	"github.com/ddev/ddev/pkg/dockerutil"
+	"github.com/docker/docker/api/types/container"
 	"os"
-
-	"github.com/ddev/ddev/cmd/ddev/cmd"
-	"github.com/ddev/ddev/pkg/amplitude"
-	"github.com/ddev/ddev/pkg/util"
 )
 
 func main() {
-	defer util.TimeTrack()()
-
-	// Initialization is currently done before via init() func somewhere while
-	// creating the ddevapp. This should be cleaned up.
-	amplitude.InitAmplitude()
-	defer func() {
-		amplitude.Flush()
-	}()
-
-	// Prevent running as root for most cases
-	// We really don't want ~/.ddev to have root ownership, breaks things.
-	if os.Geteuid() == 0 && len(os.Args) > 1 && os.Args[1] != "hostname" {
-		util.Failed("DDEV is not designed to be run with root privileges, please run as normal user and without sudo")
+	for i := 0; i < 10000; i++ {
+		ctx, client := dockerutil.GetDockerClient()
+		list, err := client.ContainerList(ctx, container.ListOptions{})
+		if err != nil {
+			println("failed ContainerList: %v", err)
+			os.Exit(5)
+		}
+		fmt.Printf("containers=%v", list)
 	}
-
-	cmd.Execute()
 }
