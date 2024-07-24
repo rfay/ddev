f16d1fd601a9c866e690966a35fecf99d16a52d7
Randy Fay <randy@randyfay.com>
2024-07-24 08:40:59 -0600

    build: training version of ddev-webserver, ignore

--- pkg/versionconstants/versionconstants.go
+++ pkg/versionconstants/versionconstants.go
@@ -11,7 +11,7 @@ var AmplitudeAPIKey = ""
 var WebImg = "ddev/ddev-webserver"
 
 // WebTag defines the default web image tag
-var WebTag = "20240708_rfay_postgresql_client_version" // Note that this can be overridden by make
+var WebTag = "20240724_rfay_training_test" // Note that this can be overridden by make
 
 // DBImg defines the default db image used for applications.
 var DBImg = "ddev/ddev-dbserver"
